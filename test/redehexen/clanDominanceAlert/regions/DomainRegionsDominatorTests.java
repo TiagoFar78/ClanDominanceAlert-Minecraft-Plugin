4fb00c802641c439fb2d3c887eeee44d2fc92aaf
Tiago Farinha <tiagoagostinhofarinha@gmail.com>
2023-08-15 23:25:48 +0100

    tests class for the dominator added

---
+++ test/redehexen/clanDominanceAlert/regions/DomainRegionsDominatorTests.java
@@ -0,0 +1,14 @@
+package redehexen.clanDominanceAlert.regions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DomainRegionsDominatorTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
